068069f35b3bc4ba807ade800cf073dde247f5b5
abu alqasim mohamad <abolkasm11@gmail.com>
2025-06-12 05:48:44 +0200

    before 39

--- lib/test.dart
+++ lib/test.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_state_manager/src/simple/get_state.dart';
 
+//before of 39
 class Test extends StatelessWidget {
   const Test({super.key});
 
