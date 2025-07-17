0d674a5bfdf59e62f7dee8770f9a19e27f145c44
abu alqasim mohamad <abolkasm11@gmail.com>
2025-07-17 05:03:00 +0200

    after 67 with weal

--- lib/controller/home_with_bottom_nav_bar_controller.dart
+++ lib/controller/home_with_bottom_nav_bar_controller.dart
@@ -3,6 +3,7 @@ import 'package:e_commerce_halfa/view/screens/favorite.dart';
 import 'package:e_commerce_halfa/view/screens/home.dart';
 import 'package:e_commerce_halfa/view/screens/profile.dart';
 import 'package:e_commerce_halfa/view/screens/settings.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 abstract class HomeWithBottomNavBarController extends GetxController {
@@ -12,12 +13,17 @@ abstract class HomeWithBottomNavBarController extends GetxController {
 class HomeWithBottomNavBarControllerImp extends HomeWithBottomNavBarController {
   final List pages = [
     HomePage(),
-    SettingsPage(),
-    CartPage(),
     ProfilePage(),
     FavoritePage(),
+    SettingsPage(),
   ];
-
+  List titleBottomAppBar = ["Home", "Profile", "Favorite", "Settings"];
+  List iconBottomNavBar = [
+    Icons.home_outlined,
+    Icons.person_outline,
+    Icons.favorite_border_outlined,
+    Icons.settings_outlined,
+  ];
   int selectedItem = 0;
 
   changeCurrentPage(int i) {
--- lib/view/widgets/home_with_bottom_nav_bar_widgets/bottom_app_bar.dart
+++ lib/view/widgets/home_with_bottom_nav_bar_widgets/bottom_app_bar.dart
@@ -30,51 +30,102 @@ class CustomBottomAppBar extends StatelessWidget {
       notchMargin: 20.0,
       child: Row(
         children: [
+          // عدد العناصر الأساسية في الـ
+          //BottomBar هو 4 Widgets
+          // أضفنا عنصر إضافي
+          //Spacer()
+          // عشان نترك فراغ في النص للـ
+          // FloatingActionButton
+          // يعني المجموع بقى 5 عناصر، عشان كده استخدمنا: +1
+          // الترتيب:
+          // [Favorite, Settings] يسار — Spacer — [Home, Profile] يمين
+          // والـ
+          //FloatingActionButton
+          // بيكون في النص بالضبط فوق الـ
+          // Spacer
+          ...List.generate(homeWithBottomNavBarController.pages.length + 1, (
+            index,
+          ) {
+            // نستخدم هذا الشرط عشان نتجاوز الفراغ
+            //(Spacer)
+            // الموجود في
+            //index 2
+            // لأننا أضفنا عنصر زيادة في القائمة باستخدام +1 في
+            // List.generate
+            // وبالتالي، العناصر اللي بعد الفراغ لازم نطرح منها 1 عشان نرجع للـ
+            //index الصحيح
+            // مثال:
+            // index = 0 أو 1 → نستخدمه كما هو
+            // index = 3 → لازم نطرحه 1 عشان نصل للعنصر رقم 2
+            // index = 4 → نطرحه 1 عشان نصل للعنصر رقم 3
+
+            int i = index > 2 ? index - 1 : index;
+            //قلنا ليهو انتة لو وصلتة العنصر رقم 2 ختي لينا شنو ?
+            //spacer
+            //اي مساحة فاضية عشان نقدر نخت فيها ال
+            //floationactionbutton
+            return index == 2
+                ? Spacer()
+                : Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  child: CustomeButtonBottomNavBar(
+                    onTap: () {
+                      homeWithBottomNavBarController.changeCurrentPage(i);
+                    },
+                    icon: homeWithBottomNavBarController.iconBottomNavBar[i],
+                    title: homeWithBottomNavBarController.titleBottomAppBar[i],
+                    active:
+                        homeWithBottomNavBarController.selectedItem == i
+                            ? true
+                            : false,
+                  ),
+                );
+          }),
           //Why did he use row inside another row?
-          Row(
-            spacing: 20.0,
-            children: [
-              CustomeButtonBottomNavBar(
-                onTap: () {
-                  homeWithBottomNavBarController.changeCurrentPage(0);
-                },
-                icon: Icons.home_outlined,
-                title: 'Home',
-                active: homeWithBottomNavBarController.selectedItem == 0,
-              ),
-              CustomeButtonBottomNavBar(
-                onTap: () {
-                  homeWithBottomNavBarController.changeCurrentPage(1);
-                },
-                icon: Icons.person_outline,
-                title: 'Profile',
-                active: homeWithBottomNavBarController.selectedItem == 1,
-              ),
-            ],
-          ),
+          // Row(
+          //   spacing: 20.0,
+          //   children: [
+          //     CustomeButtonBottomNavBar(
+          //       onTap: () {
+          //         homeWithBottomNavBarController.changeCurrentPage(0);
+          //       },
+          //       icon: Icons.home_outlined,
+          //       title: 'Home',
+          //       active: homeWithBottomNavBarController.selectedItem == 0,
+          //     ),
+          //     CustomeButtonBottomNavBar(
+          //       onTap: () {
+          //         homeWithBottomNavBarController.changeCurrentPage(1);
+          //       },
+          //       icon: Icons.person_outline,
+          //       title: 'Profile',
+          //       active: homeWithBottomNavBarController.selectedItem == 1,
+          //     ),
+          //   ],
+          // ),
           // Spacer() يضيف فراغ مرن بين المجموعتين، ويدفع كل مجموعة لأطراف الشاشة
-          Spacer(),
-          Row(
-            spacing: 20.0,
-            children: [
-              CustomeButtonBottomNavBar(
-                onTap: () {
-                  homeWithBottomNavBarController.changeCurrentPage(2);
-                },
-                icon: Icons.favorite_border_outlined,
-                title: 'Favorite',
-                active: homeWithBottomNavBarController.selectedItem == 2,
-              ),
-              CustomeButtonBottomNavBar(
-                onTap: () {
-                  homeWithBottomNavBarController.changeCurrentPage(3);
-                },
-                icon: Icons.settings_outlined,
-                title: 'Settings',
-                active: homeWithBottomNavBarController.selectedItem == 3,
-              ),
-            ],
-          ),
+          // Spacer(),
+          // Row(
+          //   spacing: 20.0,
+          //   children: [
+          //     CustomeButtonBottomNavBar(
+          //       onTap: () {
+          //         homeWithBottomNavBarController.changeCurrentPage(2);
+          //       },
+          //       icon: Icons.favorite_border_outlined,
+          //       title: 'Favorite',
+          //       active: homeWithBottomNavBarController.selectedItem == 2,
+          //     ),
+          //     CustomeButtonBottomNavBar(
+          //       onTap: () {
+          //         homeWithBottomNavBarController.changeCurrentPage(3);
+          //       },
+          //       icon: Icons.settings_outlined,
+          //       title: 'Settings',
+          //       active: homeWithBottomNavBarController.selectedItem == 3,
+          //     ),
+          //   ],
+          // ),
         ],
       ),
     );
