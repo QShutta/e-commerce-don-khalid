56e82f01a36f46f7e2e7759082210cc5a7a3587b
abu alqasim mohamad <abolkasm11@gmail.com>
2025-07-21 03:50:03 +0200

    before of 71

--- lib/controller/products_controller.dart
+++ lib/controller/products_controller.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_halfa/core/constants/image_assets.dart';
 import 'package:e_commerce_halfa/data/model/catogeries_model.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
@@ -13,6 +14,15 @@ abstract class ProductsController extends GetxController {
 class ProductsControllerImp extends ProductsController {
   List<Catogeries>? catogeriesList;
   int? selectedCat;
+  List testImages = [
+    ImageAssets.bannerDonJalabye,
+    ImageAssets.bannerDonPerfume,
+
+    ImageAssets.bannerDonSdyre,
+    ImageAssets.bannerDonSuite,
+  ];
+  List testTexts = ["image1", "image2", "image3", "image4"];
+  List testPrices = ["45", "84", "94", "01"];
   @override
   getData() {}
   @override
--- lib/view/screens/products.dart
+++ lib/view/screens/products.dart
@@ -1,6 +1,5 @@
 import 'package:e_commerce_halfa/controller/products_controller.dart';
-import 'package:e_commerce_halfa/data/model/catogeries_model.dart';
-import 'package:e_commerce_halfa/view/widgets/home_widgets/catogery_list.dart';
+import 'package:e_commerce_halfa/core/constants/color_app.dart';
 import 'package:e_commerce_halfa/view/widgets/home_widgets/don_app_bar.dart';
 import 'package:e_commerce_halfa/view/widgets/proudcts_page_widgets/products_catogeries_list.dart';
 import 'package:flutter/material.dart';
@@ -23,11 +22,84 @@ class ProductsPage extends StatelessWidget {
       body: Scaffold(
         body: GetBuilder<ProductsControllerImp>(
           builder: (_) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.start,
+            return ListView(
               children: [
                 ProductCatogeryList(
                   catogeriesList: productsController.catogeriesList!,
+                ),
+                GridView.builder(
+                  itemCount: productsController.testImages.length,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    //This to controll on the heigh of the card.
+                    mainAxisExtent: 200,
+
+                    crossAxisSpacing: 2.0,
+                    crossAxisCount: 2,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: InkWell(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                              20,
+                            ), // ← زود الرقم حسب الشكل اللي تريده
+                          ),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(
+                                20,
+                              ), // ← نفس رقم التدوير
+                            ),
+                            child: Column(
+                              children: [
+                                Center(
+                                  child: SizedBox(
+                                    height: 100,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: Image.asset(
+                                        // color: Colors.red,
+                                        productsController.testImages[index],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  style: TextStyle(color: AppColor.greyText),
+                                  productsController.testTexts[index],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(
+                                        Icons.favorite_outline,
+                                        color: AppColor.primaryColor,
+                                      ),
+                                    ),
+                                    Text(
+                                      "\$${productsController.testPrices[index]}",
+                                      style: TextStyle(
+                                        color: AppColor.primaryColor,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ],
             );
