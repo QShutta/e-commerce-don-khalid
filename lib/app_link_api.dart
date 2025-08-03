f42cdb702d7fd4faa988c1a8c8453a5b7c24de0a
abu alqasim mohamad <abolkasm11@gmail.com>
2025-08-03 19:26:27 +0200

    after 85 with weal

--- lib/app_link_api.dart
+++ lib/app_link_api.dart
@@ -23,4 +23,8 @@ class AppLinkApi {
 
   //--------------------------------Products===================================
   static const String products = "$serverUrl/products/products.php";
+
+  //-------------------------------Favorite------------------------------------
+  static const String addToFav = "$serverUrl/favorite/add.php";
+  static const String deleteFav = "$serverUrl/favorite/delete.php";
 }
---
+++ lib/data/data_source/remote/favData.dart
@@ -0,0 +1,23 @@
+import 'package:e_commerce_halfa/app_link_api.dart';
+import 'package:e_commerce_halfa/core/class/crud.dart';
+
+class FavData {
+  Crud crud;
+  FavData(this.crud);
+
+  Future getData(userId, productId) async {
+    var response = await crud.postData(AppLinkApi.addToFav, {
+      "user_id": userId,
+      "product_id": productId,
+    });
+    return response.fold((l) => l, (r) => r);
+  }
+
+  Future deleteFav(userId, productId) async {
+    var response = await crud.postData(AppLinkApi.deleteFav, {
+      "user_id": userId,
+      "product_id": productId,
+    });
+    return response.fold((l) => l, (r) => r);
+  }
+}
