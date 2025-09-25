3bb846774f5e8301bdb5d85ed76b0a594d16d90f
abu alqasim mohamad <abolkasm11@gmail.com>
2025-09-25 05:19:59 +0200

    before of 140

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-//befoer of 117
+//befoer of 140
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
