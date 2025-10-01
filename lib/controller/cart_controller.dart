f61ed34f682b40c18de7ccc6845133c99855f3f6
abu alqasim mohamad <abolkasm11@gmail.com>
2025-10-01 03:59:43 +0200

    compltete 143 with weal

--- lib/controller/cart_controller.dart
+++ lib/controller/cart_controller.dart
@@ -8,7 +8,7 @@ import 'package:e_commerce_halfa/data/model/coupon_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-//mistake in commit128
+//mistake in commit143
 class CartController extends GetxController {
   //دة بحتوي ليك علي كل المنتجات القاعدة في السلة بي تفاصيلها .
   List<CartModel>? cartDetails = [];
