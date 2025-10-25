bf132ea9084a12d0546ed9ccaac9adea6f6a29a6
QShutta <abolkasm11@gmail.com>
2025-10-25 06:35:53 +0200

    before of 161

--- lib/view/screens/order/order_details.dart
+++ lib/view/screens/order/order_details.dart
@@ -19,13 +19,13 @@ class OrderDetails extends StatelessWidget {
         searchTextTitle: 'OrderDetails',
         textColor: AppColor.primaryColor,
       ),
-      body: ListView(
-        children: [
-          GetBuilder<OrderDetailsController>(
-            builder: (controller) {
-              return HnadlingDataView(
-                stautusRequest: orderController.statusRequest,
-                widget: Padding(
+      body: GetBuilder<OrderDetailsController>(
+        builder: (controller) {
+          return HnadlingDataView(
+            stautusRequest: orderController.statusRequest,
+            widget: ListView(
+              children: [
+                Padding(
                   padding: const EdgeInsets.all(12.0),
                   child: Card(
                     color: Colors.white,
@@ -112,172 +112,92 @@ class OrderDetails extends StatelessWidget {
                               );
                             },
                           ),
-                          // TableRow(
-                          //   children: [
-                          //     Text(
-                          //       "توب الدون",
-                          //       textAlign: TextAlign.center,
-                          //       style: TextStyle(color: Colors.grey),
-                          //     ),
-                          //     Text(
-                          //       "6",
-                          //       textAlign: TextAlign.center,
-                          //       style: TextStyle(color: Colors.grey),
-                          //     ),
-                          //     Text(
-                          //       "5000",
-                          //       textAlign: TextAlign.center,
-                          //       style: TextStyle(color: Colors.grey),
-                          //     ),
-                          //   ],
-                          // ),
                         ],
                       ),
                     ),
                   ),
                 ),
-              );
-            },
-          ),
-          SizedBox(height: 10),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 10.0),
+                SizedBox(height: 10),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10.0),
 
-            child: Card(
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Order Price:\$${orderController.orderModel!.orderTotalPrice}",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: AppColor.primaryColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-              color: Colors.white,
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: AppColor.primaryColor,
-                  child: Icon(Icons.location_on_outlined, color: Colors.white),
-                ),
-                title: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  width: double.infinity,
-                  child: Text(
-                    "Shipping Address",
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-                subtitle: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  width: double.infinity,
-                  child: Text(
-                    orderController.orderModel!.addressName == null
-                        ? "No address data"
-                        : "${orderController.orderModel!.addressName}\n${orderController.orderModel!.addressCity}\n${orderController.orderModel!.addressStreet}\n",
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Card(
-            color: Colors.white,
-            child: Container(
-              margin: EdgeInsets.all(12),
-              width: 300,
-              height: 300,
-              child: FlutterMap(
-                mapController: orderController.mapController,
-                options: MapOptions(
-                  onTap: (tapPosition, latLang) {},
-                  initialCenter: LatLng(
-                    //This to handle the case of the user chose to pickup from the store.
-                    //In that case ,will just display default location
-                    orderController.orderModel!.orderDeliveryType == 1
-                        ? orderController.storeLat
-                        : orderController.orderModel!.addressLat!,
-                    orderController.orderModel!.orderDeliveryType == 1
-                        ? orderController.storeLang
-                        : orderController.orderModel!.addressLang!,
-                  ),
-                  initialZoom: 14,
-                ),
-                children: [
-                  //1️⃣ Tile = البلاطة
-                  // الخريطة الكبيرة ما بتنزل كاملة في صورة واحدة، لأن دا حيكون تقيل على التطبيق والموبايل.
-                  // عشان كدا، الخريطة بتتقسم لمربعات صغيرة اسمها Tiles (يعني بلاطات).
-                  // كل Tile
-                  //هو جزء صغير من الخريطة.
-                  // التطبيق بيجيب كل البلاطات دي من السيرفر ويجمعها عشان تظهر ليك الخريطة كاملة على الشاشة
-                  TileLayer(
-                    // Bring your own tiles
-                    //هنا انت بتحدد السيرفر اللي حيجيب منه البلاطات.
-
-                    // {z} = مستوى الزوم (Zoom level)
-                    // {x} = رقم البلاطة أفقياً
-                    // {y} = رقم البلاطة عمودياً
-                    // يعني التطبيق يعرف من وين يجيب كل بلاطة عشان يعمل الخريطة الكبيرة.
-                    urlTemplate:
-                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
-                    userAgentPackageName:
-                        'com.e_commerce_halfa', // Add your app identifier
-                    // And many more recommended properties!
-                  ),
-                  //القوانين بتاعة الخرائط
-                  //(زي OpenStreetMap)
-                  // بتقول:
-                  // لو إنت استخدمت خريطتهم في تطبيقك لازم تكتب مصدر الخريطة
-                  //(Attribution) في مكان واضح.
-                  //So this widget "RichAttributionWidget" will do that job it will
-                  //Dispaly the the text "OpenStreetMap" contributors in the middle of the screen as a link when click
-                  //on it will take you to there website.
-                  RichAttributionWidget(
-                    // Include a stylish prebuilt attribution widget that meets all requirments
-                    attributions: [
-                      TextSourceAttribution(
-                        'OpenStreetMap contributors',
-                        onTap:
-                            () => launchUrl(
-                              Uri.parse('https://openstreetmap.org/copyright'),
-                            ), // (external)
+                  child: Card(
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Order Price:\$${orderController.orderModel!.orderTotalPrice}",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: AppColor.primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
                       ),
-                      // Also add images...
-                    ],
+                    ),
                   ),
-                  // ✅ نعرض العلامة المستخدم ضغط عليها هنا
-                  // GetBuilder<AddAddressController>(
-                  //   builder:
-                  //       (controller) =>
-                  //           MarkerLayer(markers: controller.markerList),
-                  // ),
-                  //دة عشان يحط علامة حمرا في الموقع الحالي للمستخدم.
-                  //وانا في الاضافة ما عاوزو يخت علامة حمرا في موقع المستخدم احلالي
-                  MarkerLayer(
-                    markers: [
-                      Marker(
-                        width: 100,
-                        height: 100,
-                        point: LatLng(
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    color: Colors.white,
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: AppColor.primaryColor,
+                        child: Icon(
+                          Icons.location_on_outlined,
+                          color: Colors.white,
+                        ),
+                      ),
+                      title: Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 5,
+                        ),
+                        width: double.infinity,
+                        child: Text(
+                          "Shipping Address",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      subtitle: Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 5,
+                        ),
+                        width: double.infinity,
+                        child: Text(
+                          orderController.orderModel!.addressName == null
+                              ? "No address data"
+                              : "${orderController.orderModel!.addressName}\n${orderController.orderModel!.addressCity}\n${orderController.orderModel!.addressStreet}\n",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  child: Container(
+                    margin: EdgeInsets.all(12),
+                    width: 300,
+                    height: 300,
+                    child: FlutterMap(
+                      mapController: orderController.mapController,
+                      options: MapOptions(
+                        onTap: (tapPosition, latLang) {},
+                        initialCenter: LatLng(
                           //This to handle the case of the user chose to pickup from the store.
                           //In that case ,will just display default location
                           orderController.orderModel!.orderDeliveryType == 1
@@ -287,18 +207,92 @@ class OrderDetails extends StatelessWidget {
                               ? orderController.storeLang
                               : orderController.orderModel!.addressLang!,
                         ),
-                        child: Icon(
-                          Icons.location_on_rounded,
-                          color: Colors.red,
-                        ),
+                        initialZoom: 14,
                       ),
-                    ],
+                      children: [
+                        //1️⃣ Tile = البلاطة
+                        // الخريطة الكبيرة ما بتنزل كاملة في صورة واحدة، لأن دا حيكون تقيل على التطبيق والموبايل.
+                        // عشان كدا، الخريطة بتتقسم لمربعات صغيرة اسمها Tiles (يعني بلاطات).
+                        // كل Tile
+                        //هو جزء صغير من الخريطة.
+                        // التطبيق بيجيب كل البلاطات دي من السيرفر ويجمعها عشان تظهر ليك الخريطة كاملة على الشاشة
+                        TileLayer(
+                          // Bring your own tiles
+                          //هنا انت بتحدد السيرفر اللي حيجيب منه البلاطات.
+
+                          // {z} = مستوى الزوم (Zoom level)
+                          // {x} = رقم البلاطة أفقياً
+                          // {y} = رقم البلاطة عمودياً
+                          // يعني التطبيق يعرف من وين يجيب كل بلاطة عشان يعمل الخريطة الكبيرة.
+                          urlTemplate:
+                              'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
+                          userAgentPackageName:
+                              'com.e_commerce_halfa', // Add your app identifier
+                          // And many more recommended properties!
+                        ),
+                        //القوانين بتاعة الخرائط
+                        //(زي OpenStreetMap)
+                        // بتقول:
+                        // لو إنت استخدمت خريطتهم في تطبيقك لازم تكتب مصدر الخريطة
+                        //(Attribution) في مكان واضح.
+                        //So this widget "RichAttributionWidget" will do that job it will
+                        //Dispaly the the text "OpenStreetMap" contributors in the middle of the screen as a link when click
+                        //on it will take you to there website.
+                        RichAttributionWidget(
+                          // Include a stylish prebuilt attribution widget that meets all requirments
+                          attributions: [
+                            TextSourceAttribution(
+                              'OpenStreetMap contributors',
+                              onTap:
+                                  () => launchUrl(
+                                    Uri.parse(
+                                      'https://openstreetmap.org/copyright',
+                                    ),
+                                  ), // (external)
+                            ),
+                            // Also add images...
+                          ],
+                        ),
+                        // ✅ نعرض العلامة المستخدم ضغط عليها هنا
+                        // GetBuilder<AddAddressController>(
+                        //   builder:
+                        //       (controller) =>
+                        //           MarkerLayer(markers: controller.markerList),
+                        // ),
+                        //دة عشان يحط علامة حمرا في الموقع الحالي للمستخدم.
+                        //وانا في الاضافة ما عاوزو يخت علامة حمرا في موقع المستخدم احلالي
+                        MarkerLayer(
+                          markers: [
+                            Marker(
+                              width: 100,
+                              height: 100,
+                              point: LatLng(
+                                //This to handle the case of the user chose to pickup from the store.
+                                //In that case ,will just display default location
+                                orderController.orderModel!.orderDeliveryType ==
+                                        1
+                                    ? orderController.storeLat
+                                    : orderController.orderModel!.addressLat!,
+                                orderController.orderModel!.orderDeliveryType ==
+                                        1
+                                    ? orderController.storeLang
+                                    : orderController.orderModel!.addressLang!,
+                              ),
+                              child: Icon(
+                                Icons.location_on_rounded,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
